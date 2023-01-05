b57ce62390c9cf327f5793251a5cdc27deffadf8
Cyphen <dbdxor@gmail.com>
2023-01-05 15:55:33 +0900

    23.01.05
    work-school

---
+++ React-native/Project/components/Greetings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Greetings() {
+  return (
+    <View>
+      <Text>안녕하세요!</Text>
+    </View>
+  );
+}
+
+export default Greetings;
