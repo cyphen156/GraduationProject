ce04841b10c10caf9c9cd7ee194ead19ca2146b3
Cyphen <dbdxor@gmail.com>
2023-01-06 15:44:18 +0900

    23.01.06
    work at school

---
+++ React-native/Project/components/Box.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+function Box() {
+  return <View style={styles.box} />;
+}
+
+const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    width: 64,
+    height: 64,
+    backgroundColor: 'black',
+  },
+});
+
+export default Box;
