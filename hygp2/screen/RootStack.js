ffd1b7254e183f0f4f2923fe18b390c5062d72b5
Cyphen <dbdxor@gmail.com>
2023-02-14 16:02:32 +0900

    23.02.14
    work at school with labtop
    fuck that gradle
    somebody kill gradle

--- hygp2/screen/RootStack.js
+++ hygp2/screen/RootStack.js
@@ -1,12 +1,14 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainTab from "./MainTab";
 import WriteScreen from './Write';
+import SignIn from "./SignIn";
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
     return (
         <Stack.Navigator>
+            <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}} />
             <Stack.Screen name="MainTab" component={MainTab} options={{headerShown: false}} />
             <Stack.Screen name="Write" component={WriteScreen} options={{headerShown: false}}/>
         </Stack.Navigator>
