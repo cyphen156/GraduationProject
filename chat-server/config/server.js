9cc9f6b36e1eb8cc551c74c22f1fd1d6079eaf0a
Cyphen <dbdxor@gmail.com>
2023-04-06 11:12:00 +0900

    23.04.06
    server init

--- chat-server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a95c33751c112cf478f5b8694a493989'),
-    },
-  },
-});
--- chat-server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '020c19ce-5913-47f7-9660-8d81e14d36d1'
-};
