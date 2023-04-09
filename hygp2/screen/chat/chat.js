f3222d8f568148e17231f5fe6281b4e2aa624e84
Cyphen <dbdxor@gmail.com>
2023-04-10 02:00:46 +0900

    23.04.10
    asd

--- hygp2/screen/chat/chat.js
+++ hygp2/screen/chat/chat.js
@@ -1,4 +1,40 @@
-import { StyleSheet } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import io from 'socket.io-client';
 
-const [steState, useState] = useState();
+const socket = io('http://localhost:3000');
+
+function chat() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Fetch initial chat messages
+    socket.emit('fetch_messages');
+
+    // Listen for new messages
+    socket.on('new_message', (message) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, message),
+      );
+    });
+  }, []);
+
+  const onSend = (newMessages = []) => {
+    socket.emit('send_message', newMessages[0]);
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages),
+    );
+  };
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(newMessages) => onSend(newMessages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  );
+}
+
+export default chat;
