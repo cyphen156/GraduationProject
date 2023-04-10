65e93f45d7e65da8c5c4df0c318e69382543818f
Cyphen <dbdxor@gmail.com>
2023-04-10 15:23:23 +0900

    23.04.10
    yto

--- backEnd/nodeServer/server.js
+++ backEnd/nodeServer/server.js
@@ -1,56 +1,113 @@
 //모듈임포트
-const express = require('express');
-const mysql = require('mysql');
+const express = require('express'); //express 웹 서버 생성
+const morgan = require('morgan'); //로그 기록
+const http = require('http'); //http
+const cors = require('cors'); //client ajex 요청 다중서버 접속
+const firebase = require("firebase/app");
+require('firebase/firestore');
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOWnBCSHjMGCGF1EUycGntildV844uUSM",
+  authDomain: "hygp2-ec607.firebaseapp.com",
+  projectId: "hygp2-ec607",
+  storageBucket: "hygp2-ec607.appspot.com",
+  messagingSenderId: "603930050293",
+  appId: "1:603930050293:web:4acfa6e947d76587bacfbc",
+  measurementId: "G-YV0BZHL37W"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Create a Firestore reference
+const firestore = firebase.firestore();
+
 
 //앱 선언, 사용할 포트
 const app = express();
 const port = 3000;
-
-//db연결 세팅
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'chatdb'
-});
-//db연결
-connection.connect(err => {
-  if (err) throw err;
-  console.log('MySQL connected');
-});
+const server = http.createServer(app);
+app.use(cors());
 
 //함수 선언
-app.use((request, response) => {
-  response.send('<h1>HelloWofdsdfsdf</h1>');
+app.use(morgan('dev'));
+app.use((request, res) => {
+  res.send('<h1>HelloWdfdf</h1>');
 });
 
+// 로그 기록
+if (process.env.NODE_ENV === 'production') { 
+   app.use(morgan('combined')); // 배포환경
+} else {
+   app.use(morgan('dev')); // 개발환경
+}
 
+// Set up API routes
+const router = express.Router();
 
-// 라우팅경로 정의
-app.get('/chat', (req, res) => {
-  // Select all chat messages from the database
-  connection.query('SELECT * FROM chat', (err, results) => {
-    if (err) throw err;
-    res.json(results);
+// Set up the API routes
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the chat app API' });
+});
+
+router.get('/messages', async (req, res) => {
+  try {
+    const messages = [];
+    const snapshot = await firestore
+      .collection('messages')
+      .orderBy('timestamp', 'desc')
+      .limit(50)
+      .get();
+
+    snapshot.forEach((doc) => {
+      messages.push({
+        id: doc.id,
+        message: doc.data().message,
+        timestamp: doc.data().timestamp.toDate(),
+      });
+    });
+
+    res.json(messages);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.use('/api', router);
+
+////소켓
+//소켓 모듈 선언
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+//io리스너 이벤트 설정
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Listen for new chat messages
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+
+    // 콜렉션 저장
+    firestore.collection('messages').add({
+      message: msg,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    // 클라이언트에게 메세지 전송
+    io.emit('chat message', msg);
+  });
+
+  // Disconnect event
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
-/*
-// Use body-parser middleware
-app.use(bodyParser.json());
 
-app.post('/chat', (req, res) => {
-  // Insert a new chat message into the database
-  const { user, message } = req.body;
-  connection.query('INSERT INTO chat (user, message) VALUES (?, ?)', [user, message], (err, result) => {
-    if (err) throw err;
-    res.json({ id: result.insertId, user, message });
-  });
-});
 
-// Start the server
-app.listen(3000, () => console.log('Server started on port 3000'));
-*/
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
--- hygp2/screen/chat/chat.js
+++ hygp2/screen/chat/chat.js
@@ -2,38 +2,62 @@ import { useState, useEffect } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const port = 'http://localhost:3000'
+const socket = io(port);
 
 function chat() {
+  const [socket, setSocket] = useState(null);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    // Fetch initial chat messages
-    socket.emit('fetch_messages');
-
-    // Listen for new messages
-    socket.on('new_message', (message) => {
-      setMessages((previousMessages) =>
-        GiftedChat.append(previousMessages, message),
-      );
-    });
-  }, []);
-
-  const onSend = (newMessages = []) => {
-    socket.emit('send_message', newMessages[0]);
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages),
-    );
-  };
-
-  return (
-    <GiftedChat
-      messages={messages}
-      onSend={(newMessages) => onSend(newMessages)}
-      user={{
-        _id: 1,
-      }}
-    />
+     // Connect to the server
+     const socket = io(SERVER_URL);
+     setSocket(socket);
+ 
+     // Listen for new chat messages
+     socket.on('chat message', (msg) => {
+       const newMessage = {
+         _id: msg.id,
+         text: msg.message,
+         createdAt: new Date(msg.timestamp),
+         user: {
+           _id: 2,
+           name: 'React Native',
+           avatar: 'https://placeimg.com/140/140/any',
+         },
+       };
+       setMessages((prevMessages) => [newMessage, ...prevMessages]);
+     });
+ 
+     // Disconnect event
+     return () => {
+       socket.disconnect();
+     };
+   }, []);
+ 
+   const onSend = (newMessages = []) => {
+     // Send the chat message to the server
+     const message = newMessages[0];
+     socket.emit('chat message', message.text);
+ 
+     // Add the message to the local state
+     const newMessage = {
+       _id: message._id,
+       text: message.text,
+       createdAt: message.createdAt,
+       user: message.user,
+     };
+     setMessages((prevMessages) => [newMessage, ...prevMessages]);
+   };
+   return (
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        onSend={onSend}
+        user={{ _id: 1 }}
+        inverted={true}
+      />
+    </View>
   );
 }
 
