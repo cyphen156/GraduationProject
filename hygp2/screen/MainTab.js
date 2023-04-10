f0f66fe2723435aa40568220e57edd5561dacaaf
Cyphen <dbdxor@gmail.com>
2023-04-10 15:22:53 +0900

    Revert "s"
    This reverts commit 65a1ef0ecec3b7487b17d9a8bab176026e659649.

--- hygp2/screen/MainTab.js
+++ hygp2/screen/MainTab.js
@@ -12,8 +12,6 @@ import HomeStack from "./HomeStack";
 import MyProfileStack from "./MyProfileStack";
 import SubTab from "./SubTab";
 import FileUpload from "../components/FileUpload";
-import { friends } from "../Styles/friends";
-import List from "../components/Friends/List";
 
 //import GoogleSigninBTN from "../SignIn/GoogleSignin";
 
@@ -70,9 +68,6 @@ function MainTab() {
             <Tab.Screen name="FileUpload" component={FileUpload} options={{
                 tabBarIcon: ({color, size}) => (<Icon name="view-stream" size={size} color={color} />),
             }}/>
-            <Tab.Screen name="Friends" component={List} options={{
-                tabBarIcon: ({color}) => (<Icon name="person" color={color} />),
-            }}/>  
         </Tab.Navigator>
     );
 }
