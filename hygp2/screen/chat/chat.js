9222c6deda0dfcd25e476a60ff330f9c3a5da541
Cyphen <dbdxor@gmail.com>
2023-04-14 10:46:01 +0900

    asdfasdf
    asdf

--- hygp2/screen/chat/chat.js
+++ hygp2/screen/chat/chat.js
@@ -3,22 +3,15 @@ import { View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/firestore';
-import { auth } from '@react-native-firebase/auth';
 
 const firestore = firebase.firestore();
 const chatMessagesRef = firestore.collection('chatMessages');
-const recipientId = 'qwe';
+
 function chat() {
   const [messages, setMessages] = useState([]);
 
-  const [currentUser, setCurrentUser] = useState([]);
   useEffect(() => {
-    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-    });
-
     const unsubscribe = chatMessagesRef
-      .where('recipientId', '==', recipientId)
       .orderBy('createdAt', 'desc')
       .onSnapshot((querySnapshot) => {
         const newMessages = querySnapshot.docs.map((doc) => {
@@ -34,31 +27,25 @@ function chat() {
       });
 
     return () => {
-      unsubscribeAuth();
       unsubscribe();
     };
-  }, [recipientId]);
+  }, []);
  
   const onSend = useCallback((newMessages = []) => {
-    if (!currentUser) {
-      return;
-    }
-
     const message = newMessages[0];
     chatMessagesRef.add({
       text: message.text,
       createdAt: firebase.firestore.Timestamp.fromDate(message.createdAt),
-      user: { ...message.user, _id: currentUser.uid },
-      recipientId,
+      user: message.user,
     });
-  }, [currentUser, recipientId]);
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <GiftedChat
         messages={messages}
         onSend={onSend}
-        user={{ _id: currentUser ? currentUser.uid : null }}
+        user={{ _id: 1 }}
         inverted={true}
       />
     </View>
