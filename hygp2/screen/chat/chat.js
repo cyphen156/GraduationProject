5ed32d5c6c91cb725cd6c93ce45b67fbb3c66cb2
Cyphen <dbdxor@gmail.com>
2023-04-16 14:59:59 +0900

    23.04.16
    채팅 완성된건가? 일단 유저 식별하게 만들긴햇는데

--- hygp2/screen/chat/chat.js
+++ hygp2/screen/chat/chat.js
@@ -1,16 +1,21 @@
 import { useState, useEffect, useCallback } from 'react';
 import { View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Avatar, Send, SystemMessage, Bubble } from 'react-native-gifted-chat';
 import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/auth';
 import '@react-native-firebase/firestore';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const firestore = firebase.firestore();
-const chatMessagesRef = firestore.collection('chatMessages');
+const auth = firebase.auth();
 
-function chat() {
+function Chat({ chatRoomName }) {
   const [messages, setMessages] = useState([]);
+  const [senders, setSenders] = useState([]);
+  const currentUser = auth.currentUser; // 현재 로그인한 사용자 정보 가져오기
 
   useEffect(() => {
+    const chatMessagesRef = firestore.collection('chatRooms').doc(chatRoomName).collection('messages');
     const unsubscribe = chatMessagesRef
       .orderBy('createdAt', 'desc')
       .onSnapshot((querySnapshot) => {
@@ -29,27 +34,111 @@ function chat() {
     return () => {
       unsubscribe();
     };
-  }, []);
- 
+  }, [chatRoomName]);
+
+  useEffect(() => {
+    const chatUsersRef = firestore.collection('chatRooms').doc(chatRoomName).collection('users');
+    const unsubscribe = chatUsersRef.onSnapshot((querySnapshot) => {
+      const newSenders = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          _id: doc.id,
+          name: data.name,
+          avatar: data.avatar,
+        };
+      });
+      setSenders(newSenders);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [chatRoomName]);
+
   const onSend = useCallback((newMessages = []) => {
+    const chatMessagesRef = firestore.collection('chatRooms').doc(chatRoomName).collection('messages');
     const message = newMessages[0];
     chatMessagesRef.add({
       text: message.text,
       createdAt: firebase.firestore.Timestamp.fromDate(message.createdAt),
-      user: message.user,
+      user: {
+        _id: currentUser.uid, // 현재 로그인한 사용자의 UID 사용
+        name: currentUser.displayName,
+        avatar: currentUser.photoURL,
+      },
     });
-  }, []);
+  }, [chatRoomName, currentUser]);
 
   return (
     <View style={{ flex: 1 }}>
       <GiftedChat
         messages={messages}
         onSend={onSend}
-        user={{ _id: 1 }}
+        user={{
+          _id: currentUser.uid,
+          name: currentUser.displayName,
+          avatar: currentUser.photoURL,
+        }}
         inverted={true}
+        renderAvatarOnTop={true}
+        showUserAvatar={true}
+        messagesContainerStyle={{ backgroundColor: '#f0f0f0' }}
+        listViewProps={{
+          style: {
+            backgroundColor: '#f0f0f0',
+          },
+        }}
+        renderUsernameOnMessage={true}
+        renderAvatar={(props) => {
+          const sender = senders.find((s) => s._id === props.currentMessage.user._id);
+          if (sender._id === currentUser.uid) {
+            return <Avatar {...props} name="나" />;
+          } else {
+            return <Avatar {...props} avatar={sender.avatar} />;
+          }
+        }}
+        renderSend={(props) => {
+          return (
+            <Send {...props}>
+              <View style={{ marginRight: 10, marginBottom: 5 }}>
+                <Ionicons name="ios-send" size={32} color="#0084ff" />
+              </View>
+            </Send>
+          );
+        }}
+        renderSystemMessage={(props) => {
+          return (
+            <SystemMessage
+              {...props}
+              textStyle={{ color: '#f0f0f0' }}
+            />
+          );
+        }}
+        renderBubble={(props) => {
+          return (
+            <Bubble
+              {...props}
+              wrapperStyle={{
+                left: {
+                  backgroundColor: '#f0f0f0',
+                },
+                right: {
+                  backgroundColor: '#0084ff',
+                },
+              }}
+              textStyle={{
+                left: {
+                  color: '#000',
+                },
+                right: {
+                  color: '#fff',
+                },
+              }}
+            />
+          );
+        }}
       />
     </View>
   );
-}
-
-export default chat;
+};
+export default Chat;
