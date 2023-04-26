77cac07255b66218c7d094599b6ba8493e08159a
Cyphen <dbdxor@gmail.com>
2023-04-26 17:10:52 +0900

    asdf
    yarn add react-native-calendars

--- hygp2/components/CalendarView.js
+++ hygp2/components/CalendarView.js
@@ -1,43 +1,36 @@
 import { Calendar } from "react-native-calendars";
 import { StyleSheet } from "react-native";
 
-function CalendarView({markedDates, selectedDate, onSelectDate}) {
+function CalendarView({ markedDates, selectedDate, onSelectDate }) {
     const markedSelectedDate = {
       ...markedDates,
       [selectedDate]: {
+        ...markedDates[selectedDate],
         selected: true,
-        marked: markedDates[selectedDate]?.marked,
+        textColor: "#ffffff",
       },
     };
-
+  
     return (
-        <Calendar 
-            style={styles.calendar}
-            markedDates={markedSelectedDate}
-            markingType={'multi-period'}
-            onDayPress={day => {
-                onSelectDate(day.dateString);
-            }}
-            theme={{
-                backgroundColor: '#ffffff',
-                calendarBackground: '#ffffff',
-                textSectionTitleColor: '#b6c1cd',
-                selectedDayBackgroundColor: '#00adf5',
-                selectedDayTextColor: '#ffffff',
-                todayTextColor: '#00adf5',
-                dayTextColor: '#2d4150',
-                textDisabledColor: '#d9e',
-                dotColor: '#aaaaaa',
-            }} 
-        />
-    );
+      <Calendar
+        markedDates={markedSelectedDate}
+        markingType={"multi-period"}
+        onDayPress={(day) => {
+            onSelectDate(day.dateString);
+        }}
+        theme={{
+            backgroundColor: "#ffffff",
+            calendarBackground: "#ffffff",
+            textSectionTitleColor: "#b6c1cd",
+            selectedDayBackgroundColor: "#00adf5",
+            selectedDayTextColor: "#ffffff",
+            todayTextColor: "#00adf5",
+            dayTextColor: "#2d4150",
+            textDisabledColor: "#d9e",
+            dotColor: "#aaaaaa",
+        }}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-    calendar:{
-        borderBottomColor: "#e0e0e0",
-        borderBottomWidth: 1,
-    },
-});
 
 export default CalendarView;
--- hygp2/screen/Calendars/TeamCalendar.js
+++ hygp2/screen/Calendars/TeamCalendar.js
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+import { eachDayOfInterval, format, startOfDay } from 'date-fns';
 import { useState, useMemo, useEffect, useContext } from 'react';
 import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import firebase from '@react-native-firebase/app';
@@ -13,6 +13,7 @@ function TeamCalendar({ navigation }) {
   const { teamId } = useContext(TeamContext);
   const [todos, setTodos] = useState([]);
   const [selectedDate, setSelectedDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+  const colors = ["#00adf5", "#f0e68c", "#5f9ea0", "#ffa500"]; // 다양한 색상 배열 추가
 
   useEffect(() => {
     const teamsRef = firestore.collection('teams');
@@ -21,55 +22,130 @@ function TeamCalendar({ navigation }) {
         title: doc.data().name,
       });
     });
-    fetchTodos();
+  
+    const todosRef = firestore.collection(`teams`).doc(teamId).collection("todos");
+    const unsubscribeTodos = todosRef.onSnapshot((querySnapshot) => {
+      const fetchedTodos = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        fetchedTodos.push({
+          ...data,
+          startDate: data.startDate.toDate(),
+          endDate: data.endDate.toDate(),
+        });
+      });
+  
+      setTodos(fetchedTodos);
+    });
+  
     return () => {
       unsubscribe();
+      unsubscribeTodos();
     };
   }, [teamId, navigation]);
 
-  const fetchTodos = async () => {
-    // 모든 팀원의 Todos 가져오기
-    const todosRef = firestore.collection(`teams`).doc(teamId).collection("todos");
+  // const fetchTodos = async () => {
+  //   // 모든 팀원의 Todos 가져오기
+  //   const todosRef = firestore.collection(`teams`).doc(teamId).collection("todos");
   
-    const querySnapshot = await todosRef.get();
+  //   const querySnapshot = await todosRef.get();
   
-    const fetchedTodos = [];
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      fetchedTodos.push({
-        ...data,
-        startDate: data.startDate.toDate(),
-        endDate: data.endDate.toDate(),
+  //   const fetchedTodos = [];
+  //   querySnapshot.forEach((doc) => {
+  //     const data = doc.data();
+  //     fetchedTodos.push({
+  //       ...data,
+  //       startDate: data.startDate.toDate(),
+  //       endDate: data.endDate.toDate(),
+  //     });
+  //   });
+  
+  //   setTodos(fetchedTodos);
+  // };
+  // 하루만 쓸때 사용하던 캘린더 마킹
+  // const markedDates = useMemo(
+  //   () =>
+  //     todos.reduce((acc, current) => {
+  //       const formattedDate = format(new Date(current.startDate), 'yyyy-MM-dd');
+  //       acc[formattedDate] = { marked: true };
+  //       return acc;
+  //     }, {}),
+  //   [todos],
+  // );
+
+  // 시작일과 종료일사이 interver 마킹 
+  const markedDates = useMemo(() => {
+    return todos.reduce((acc, current, index) => {
+      const interval = eachDayOfInterval({
+        start: current.startDate,
+        end: current.endDate,
       });
-    });
   
-    setTodos(fetchedTodos);
-  };
+      const color = colors[index % colors.length]; // 각 할 일에 대해 다른 색상 할당
+  
+      interval.forEach((date, i) => {
+        const formattedDate = format(date, "yyyy-MM-dd");
+        if (!acc[formattedDate]) {
+          acc[formattedDate] = { periods: [], marked: false };
+        }
+  
+        if (acc[formattedDate].periods.length < 2) {
+          if (i === 0 && i === interval.length - 1) {
+            acc[formattedDate].marked = true;
+          } else if (i === 0) {
+            acc[formattedDate].periods.push({
+              startingDay: true,
+              endingDay: false,
+              color: color,
+              textColor: "#ffffff",
+            });
+          } else if (i === interval.length - 1) {
+            acc[formattedDate].periods.push({
+              startingDay: false,
+              endingDay: true,
+              color: color,
+              textColor: "#ffffff",
+            });
+          } else {
+            acc[formattedDate].periods.push({
+              color: color,
+              textColor: "#ffffff",
+            });
+          }
+        }
+      });
+  
+      return acc;
+    }, {});
+  }, [todos]);
+  
+  const filteredTodos = todos.filter((todo) => {
+    const currentDate = startOfDay(new Date(selectedDate));
+    const startDate = startOfDay(todo.startDate);
+    const endDate = startOfDay(todo.endDate);
+  
+    return currentDate >= startDate && currentDate <= endDate;
+  });
 
-  const markedDates = useMemo(
-    () =>
-      todos.reduce((acc, current) => {
-        const formattedDate = format(new Date(current.startDate), 'yyyy-MM-dd');
-        acc[formattedDate] = { marked: true };
-        return acc;
-      }, {}),
-    [todos],
-  );
-
-  const filteredTodos = todos.filter(
-    (todo) => format(todo.startDate, 'yyyy-MM-dd') === selectedDate,
-  );
-
-  const onDateSelect = (date) => {
-    setSelectedDate(date.dateString);
-  };
+  // const onDateSelect = (date) => {
+  //   setSelectedDate(date.dateString);
+  // };
 
   return (
     <View style={styles.container}>
-      <CalendarView markedDates={markedDates} onDayPress={onDateSelect} />
-      <ScrollView>
+      {/* <View style={styles.calendarContainer}> */}
+        <CalendarView
+          markedDates={markedDates}
+          selectedDate={selectedDate}
+          onSelectDate={setSelectedDate}
+        />
+      {/* </View> */}
+      <ScrollView contentContainerStyle={styles.scrollView}>
         {filteredTodos.map((todo, index) => (
-          <TouchableOpacity key={index} onPress={() => navigation.navigate('TodoDetail', { todo })}>
+          <TouchableOpacity
+            key={index}
+            onPress={() => navigation.navigate("TodoDetail", { todo })}
+          >
             <View style={styles.todoItem}>
               <Text style={styles.todoTitle}>{todo.task}</Text>
               <Text style={styles.todoText}>작업자: {todo.worker}</Text>
@@ -84,6 +160,12 @@ function TeamCalendar({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  calendarContainer: {
+    flex: 1,
+  },
+  scrollView: {
+    flexGrow: 1,
   },
   todoItem: {
     borderBottomWidth: 1,
