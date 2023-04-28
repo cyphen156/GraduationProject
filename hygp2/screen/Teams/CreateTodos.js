6d5ead0ab09f1039dc3109f8c24052e2b049e231
wjsaudwo999 <125464767+wjsaudwo999@users.noreply.github.com>
2023-04-28 13:07:06 +0900

    팀 생성 오류 작업
    수정과 오류 작업

--- hygp2/screen/Teams/CreateTodos.js
+++ hygp2/screen/Teams/CreateTodos.js
@@ -1,74 +1,53 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
 import '@react-native-firebase/firestore';
-import IconRightButton from '../../components/IconRightButton';
-import IconLeftButton from '../../components/IconLeftButton';
 import { useEffect } from 'react';
 import WriteTeamTodos from '../../components/WriteTeamTodos';
 import TeamContext from './TeamContext';
 import { useUserContext } from '../../context/UserContext';
 
 const firestore = firebase.firestore();
-const auth = firebase.auth();
 
 function CreateTodos({ navigation }) {
 
   const {user}= useUserContext();
   const { teamId } = useContext(TeamContext);
-  const [task, setTask] = useState();
-  const [discription, setDiscription] = useState();
+  const [task, setTask] = useState('');
+  const [discription, setDiscription] = useState('');
   const [endDate, setEndDate] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
   
   useEffect(() => {
     navigation.setOptions({
-        title: 'CreateTodos', headerTitleAlign: 'center',
-        headerLeft: () => (
-          <>
-            <IconLeftButton
-                name="Profile"
-                onPress={() => navigation.navigate('Profile')
-              }
-            />
-          </>),
-        headerRight: () => (
-          <View style={{flexDirection: 'row'}}>
-            <IconRightButton
-              name="search"
-              onPress={() => navigation.navigate('FriendsList')}
-              />
-            <IconRightButton
-              name="person-add"
-              onPress={() => navigation.navigate('FriendsAdd')}
-            />
-            <IconRightButton
-              name="settings"
-              onPress={() => navigation.navigate('Setting')}
-            />      
-          </View>   
-        ),
+      headerTitleAlign: 'center',
     });
 
   }, [teamId, navigation]);
   
   const saveTodo = async () => {
+    if(startDate > endDate){
+      Alert.alert("잘못된 날짜 입니다.");
+      return;
+    }
+    if(task === '' && discription === ''){
+      Alert.alert("제목과 설명을 작성하세요.");
+      return;
+    }
     const startDateTimestamp = firebase.firestore.Timestamp.fromDate(startDate);
     const endDateTimestamp = firebase.firestore.Timestamp.fromDate(endDate);
     const todosRef = firestore.collection(`teams`).doc(teamId).collection("todos");
 
       // todoId가 없다면, 새로운 할 일을 생성한다.
+      let name = user.displayName;
       const docRef = await todosRef.add({
         task,
         discription,
-        worker: user.displayName,
+        worker: name,
         startDate: startDateTimestamp,
         endDate: endDateTimestamp,
       });
-
-      // Set the new todoId
-      setTodoId(docRef.id);
     
     // Reset fields and navigate back
     setTask('');
@@ -76,7 +55,6 @@ function CreateTodos({ navigation }) {
     setStartDate(new Date());
     setEndDate(new Date());
     navigation.goBack();
-    setTodoId(null);
   };
 
   return (
